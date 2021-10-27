e7f2133c040c85597e871900d35b182b9cda9c85
rikuimm <rikuimm@metropolia.fi>
2021-10-27 15:37:45 +0300

    add routes and controller

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,14 @@
 'use strict';
-// catController
+
+const catModel = require('../models/catModel');
+
+//const cats = catModel.cats;
+const {cats} = catModel;
+
+const cat_list_get = (req, res) => {
+    res.json(cats);
+};
+
+module.exports = {
+    cat_list_get,
+};
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const {cat_list_get} = require("../controllers/catController");
 
+router.get('/', cat_list_get);
+
+/*
 router.get('/', (req, res) => {
     res.send('From this endpoint you can get cats.')
 });
+*/
 
 router.get('/:id', (req, res) => {
     res.send('You requested a cat whose id is ' + req.params.id + '.')
