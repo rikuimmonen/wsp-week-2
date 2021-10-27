2e76b5c2fccfc154558992a0cbf9e65deddd00fa
rikuimm <rikuimm@metropolia.fi>
2021-10-27 15:10:49 +0300

    add routing stuff

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
+
 const express = require('express');
+const catRoute = require('./routes/catRoute');
+
 const app = express();
 const port = 3000;
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
+app.use('/cat', catRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
