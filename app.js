28655210dee675760af6a02fd29d303e2a3690cd
rikuimm <rikuimm@metropolia.fi>
2021-11-01 14:40:27 +0200

    add cors

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 
 const app = express();
 const port = 3000;
 
-app.use('/cat', catRoute);
+app.use(cors());
 
+app.use('/cat', catRoute);
 app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
