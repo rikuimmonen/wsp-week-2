c16680945e25dcc490dcfcde913ae78202897d52
rikuimm <rikuimm@metropolia.fi>
2021-11-03 09:59:37 +0200

    add db

--- models/catModel.js
+++ models/catModel.js
@@ -1,28 +1,22 @@
 'use strict';
-const cats = [
-    {
-        id: '1',
-        name: 'Frank',
-        birthdate: '2010-10-30',
-        weight: '5',
-        owner: '1',
-        filename: 'http://placekitten.com/400/300',
-    },
-    {
-        id: '2',
-        name: 'James',
-        birthdate: '2015-12-25',
-        weight: '11',
-        owner: '2',
-        filename: 'http://placekitten.com/400/302',
-    },
-];
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
 
 const getCat = (id) => {
-    return cats.find((cat) => cat.id === id);
+  return cats.find((cat) => cat.id === id);
 };
 
 module.exports = {
-    cats,
-    getCat,
+  getAllCats,
+  getCat,
 };
