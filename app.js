67f4081cc8dd3e8f803f442e1033921e4dd942ef
rikuimm <rikuimm@metropolia.fi>
2021-11-07 15:02:37 +0200

    add uploads folder

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('./uploads/'));
 
 app.use('/cat', catRoute);
 app.use('/user', userRoute);
